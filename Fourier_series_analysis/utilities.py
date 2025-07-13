2d44f490ce6d189f7829fab931bec312bc258fbd
D-A-Reiss <146475753+D-A-Reiss@users.noreply.github.com>
2025-07-13 21:04:34 +0200

    add get_file_names_with_version

--- Fourier_series_analysis/utilities.py
+++ Fourier_series_analysis/utilities.py
@@ -11,8 +11,8 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 """
-
-
+import glob
+import os
 import time
 import json5
 import numpy as np
@@ -23,6 +23,54 @@ from logging_config import get_logger
 
 
 logger = get_logger("utilities.py")
+
+
+def get_file_names_with_version(base_file_name: str, no_versions: int, directory: str, include_existing_versions=True,
+                                add_versions=False) -> tuple[list[str], int]:
+    """
+    Generate file names with version numbers appended to the base file name.
+
+    Parameters:
+        base_file_name: base file name without version number
+        no_versions: number of (further) versions to generate
+        directory: directory where the files are (to be) stored
+        include_existing_versions: if True, start with version 1,
+                                   otherwise with max_existing_version + 1
+        add_versions: if True or if no version exists, end with version max_existing_version + no_versions,
+                      otherwise with max_existing_version
+
+    Returns:
+        list of file names with version numbers, last version number used
+    """
+
+    base_file_name = base_file_name.split(".")
+    assert len(base_file_name) == 2, "base_file_name must be a string of the form 'name.extension'"
+
+    pattern = f"{base_file_name[0]}_v*.{base_file_name[1]}"
+
+    # find all files in the directory that match the pattern
+    file_paths = glob.glob(os.path.join(directory, pattern))
+    file_names = [os.path.basename(path) for path in file_paths]
+
+    if len(file_names) > 0:
+        # extract version numbers from existing file names
+        existing_versions = [int(name.split("_v")[1].split(".")[0]) for name in file_names]
+        max_existing_version = max(existing_versions)
+    else:
+        max_existing_version = 0
+
+    if include_existing_versions:
+        start_version = 1
+    else:
+        start_version = max_existing_version + 1
+
+    if add_versions or max_existing_version == 0:
+        end_version = max_existing_version + no_versions
+    else:
+        end_version = max_existing_version
+
+    return [f"{base_file_name[0]}_v{version_no}.{base_file_name[1]}"
+            for version_no in range(start_version, end_version + 1)], end_version
 
 
 def import_params_from_json5(path: str) -> dict:
